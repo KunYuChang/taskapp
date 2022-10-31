9dd61716a92fbe5d4bf80ef0abe1b2d57ef3ad38
KunYuChang <mischiefsub@gmail.com>
2022-10-31 21:00:39 +0800

    第一個View

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo "Hello World";
-        //return view('welcome_message');
+        echo view('Home/index.php');
     }
 }
