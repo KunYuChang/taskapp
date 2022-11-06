35ce3ecf74d1ea374eecffaaa9ec60c1deac2520
KunYuChang <mischiefsub@gmail.com>
2022-11-06 16:44:03 +0800

    使用CI hash密碼

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,11 +8,25 @@ class UserModel extends \CodeIgniter\Model
     protected $table = 'user';
 
     // (2) 設定這個 Table 允許被寫入的欄位 (CI預設不給寫入)
-    protected $allowedFields = ['name', 'email'];
+    protected $allowedFields = ['name', 'email', 'password'];
 
     // (3) https://codeigniter.com/user_guide/models/entities.html
     protected $returnType = 'App\Entities\Task';
 
     // https://codeigniter.com/user_guide/models/model.html#usetimestamps
     protected $useTimestamps = true;
+
+    // https://codeigniter.com/user_guide/models/model.html#model-events
+    protected $beforeInsert = ['hashPassword'];
+
+    protected function hashPassword(array $data)
+    {
+        if(isset($data['data']['password'])) {
+            // https://www.php.net/manual/en/function.password-hash.php
+            $data['data']['password_hash'] = password_hash($data['data']['password'],PASSWORD_DEFAULT);
+
+            unset($data['data']['password']);
+        }
+        return $data;
+    }
 }
