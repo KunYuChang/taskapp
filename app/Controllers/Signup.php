24023791ebdbf3d1b3bce3b50018c003e74ad98a
KunYuChang <mischiefsub@gmail.com>
2022-11-20 11:44:15 +0800

    寄出激活信件

--- app/Controllers/Signup.php
+++ app/Controllers/Signup.php
@@ -7,6 +7,8 @@ use App\Models\UserModel;
 
 class Signup extends BaseController
 {
+    private $token;
+
     public function new()
     {
         return view("Signup/new");
@@ -17,15 +19,19 @@ class Signup extends BaseController
         $user = new User($this->request->getPost());
         $model = new UserModel;
 
-        $token = bin2hex(random_bytes(16));
+        $this->token = bin2hex(random_bytes(16));
 
         // 密碼產生器 : https://randomkeygen.com/
-        $hash = hash_hmac('sha256', $token, $_ENV['HASH_SECRET_KEY']);
+        $hash = hash_hmac('sha256', $this->token, $_ENV['HASH_SECRET_KEY']);
 
         $user->activation_hash = $hash;
 
         if ($model->insert($user)) {
+
+            $this->sendActivationEmail($user);
+
             return redirect()->to("/signup/success");
+
         } else {
             return redirect()->back()
                 ->with('errors', $model->errors())
@@ -37,5 +43,27 @@ class Signup extends BaseController
     public function success()
     {
         return view('Signup/success');
+    }
+
+    private function sendActivationEmail($user)
+    {
+        $email = service('email');
+
+        $email->setTo($user->email);
+        $email->setSubject('Account activation');
+
+        $message = view('Signup/activation_email', [
+            'token' => $this->token
+        ]);
+
+        $email->setMessage($message);
+
+        $email->send();
+
+//        if ($email->send()) {
+//            echo "Message sent";
+//        } else {
+//            echo $email->printDebugger();
+//        }
     }
 }
---
+++ app/Views/Signup/activation_email.php
@@ -0,0 +1,5 @@
+<h1>帳號激活</h1>
+
+<p>請點擊下方連結以激活您的帳號</p>
+
+<a href="<?= site_url("signup/activate/$token")?>">激活</a>
