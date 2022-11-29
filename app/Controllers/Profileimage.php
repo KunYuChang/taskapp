d950e942753537cddd4ac4960d20ac0668e29725
KunYuChang <mischiefsub@gmail.com>
2022-11-29 14:03:37 +0800

    限制上傳文件的大小與類型

--- app/Controllers/Profileimage.php
+++ app/Controllers/Profileimage.php
@@ -13,14 +13,37 @@ class Profileimage extends BaseController
     {
         $file = $this->request->getFile('image');
 
-        if (!$file->isValid()) {
+        if ( ! $file->isValid()) {
+
             $error_code = $file->getError();
 
             if ($error_code == UPLOAD_ERR_NO_FILE) {
+
                 return redirect()->back()
-                                 ->with('warning', '沒有上傳選擇任何檔案');
+                    ->with('warning', 'No file selected');
             }
-            throw new \RuntimeException($file->getErrorString(). ' ' . $error_cod);
+
+            throw new \RuntimeException($file->getErrorString() . " " . $error_code);
         }
+
+        $size = $file->getSizeByUnit('mb');
+
+        if ($size > 2) {
+
+            return redirect()->back()
+                ->with('warning', 'File too large (max 2MB)');
+        }
+
+        $type = $file->getMimeType();
+
+        if ( ! in_array($type, ['image/png', 'image/jpeg'])) {
+
+            return redirect()->back()
+                ->with('warning', 'Invalid file format (PNG or JPEG only)');
+        }
+
+        echo $file->getClientName();
     }
 }
+
+
