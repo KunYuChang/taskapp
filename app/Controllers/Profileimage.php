45418b15a6640003dee4aab403d53eced3892ff4
KunYuChang <mischiefsub@gmail.com>
2022-11-29 11:28:07 +0800

    驗證上傳文件

--- app/Controllers/Profileimage.php
+++ app/Controllers/Profileimage.php
@@ -8,4 +8,19 @@ class Profileimage extends BaseController
     {
         return view('Profileimage/edit');
     }
+
+    public function update()
+    {
+        $file = $this->request->getFile('image');
+
+        if (!$file->isValid()) {
+            $error_code = $file->getError();
+
+            if ($error_code == UPLOAD_ERR_NO_FILE) {
+                return redirect()->back()
+                                 ->with('warning', '沒有上傳選擇任何檔案');
+            }
+            throw new \RuntimeException($file->getErrorString(). ' ' . $error_cod);
+        }
+    }
 }
